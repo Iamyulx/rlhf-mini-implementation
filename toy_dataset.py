516b29f75c1a97da07cfccf6632aa26a34548cdd
Iamyulx <yulianny.a.s14@gmail.com>
2026-03-08 21:55:49 -1100

    data

---
+++ toy_dataset.py
@@ -0,0 +1,27 @@
+data = [
+("What is malware?",
+"Malware is malicious software designed to harm systems"),
+
+("Explain phishing",
+"Phishing is a cyberattack that tricks users into revealing information"),
+
+("What is encryption?",
+"Encryption converts data into a secure format")
+]
+
+
+preferences = [
+
+{
+"prompt":"Explain phishing",
+"chosen":"Phishing is a cyber attack",
+"rejected":"Phishing is a type of fish"
+},
+
+{
+"prompt":"What is malware",
+"chosen":"Malware is malicious software",
+"rejected":"Malware is a computer mouse"
+}
+
+]
